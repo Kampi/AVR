a80c439cf6a5a0fdfce18ca44140f9f19acaeca4
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2019-06-14 14:00:13 +0200

    Typo

--- examples/LibraryExamples/SD_Example/SD_Example/main.c
+++ examples/LibraryExamples/SD_Example/SD_Example/main.c
@@ -3,7 +3,7 @@
  * 
  *  Copyright (C) Daniel Kampert, 2018
  *	Website: www.kampis-elektroecke.de
- *  File info: SD card example for XMega
+ *  File info: SD card example for XMega.
 
   GNU GENERAL PUBLIC LICENSE:
   This program is free software: you can redistribute it and/or modify
