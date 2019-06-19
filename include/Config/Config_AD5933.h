c214f51be128a5b6c5cb5e2fcdae586839873d6d
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2019-06-19 08:38:42 +0200

    Typo

--- include/Config/Config_AD5933.h
+++ include/Config/Config_AD5933.h
@@ -36,7 +36,7 @@
  #define AD5933_INTERFACE						&TWIC					/**< I2C interface for the AD5933. */
  #define AD5933_CLOCK							100000					/**< I2C clock rate for the AD5933. */
 
- #define AD5933_CALIBRATIONIMPEDANCE			1000						/**< Calibration impedance for the AD5933. */
+ #define AD5933_CALIBRATIONIMPEDANCE			1000					/**< Calibration impedance for the AD5933. */
  #define AD5933_SWEEP_LENGTH					100						/**< Length of frequency sweep */
 
 #endif /* CONFIG_AD5933_H_ */
