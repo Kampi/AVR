55fabb34cb1c0bb626c1d63325405a7dd2e8498d
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2019-09-09 15:00:56 +0200

    Typo

--- examples/LibraryExamples/AD5933_Example/AD5933_Example/source/Peripheral/AD5933/AD5933.c
+++ examples/LibraryExamples/AD5933_Example/AD5933_Example/source/Peripheral/AD5933/AD5933.c
@@ -3,7 +3,7 @@
  *
  *  Copyright (C) Daniel Kampert, 2018
  *	Website: www.kampis-elektroecke.de
- *  File info: Driver for AD5933 Impedance converter.
+ *  File info: Driver for AD5933 I2C Impedance converter.
 
   GNU GENERAL PUBLIC LICENSE:
   This program is free software: you can redistribute it and/or modify
@@ -23,7 +23,7 @@
  */
 
 /** @file Peripheral/AD5933/AD5933.c
- *  @brief Driver for the Analog Devices AD5933 impedance converter.
+ *  @brief Driver for the Analog Devices AD5933 I2C impedance converter.
  *
  *  This file contains the implementation of the impedance converter driver.
  *
