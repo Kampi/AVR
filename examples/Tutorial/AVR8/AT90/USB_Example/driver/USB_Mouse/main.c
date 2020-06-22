957153f855dd0c360cdbc9757043cfc26fecdadb
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2020-06-22 14:25:23 +0200

    Add empty project for USB mouse

--- examples/Tutorial/AVR8/AT90/USB_Example/driver/USB_Mouse/main.c
+++ examples/Tutorial/AVR8/AT90/USB_Example/driver/USB_Mouse/main.c
@@ -3,7 +3,7 @@
  * 
  *  Copyright (C) Daniel Kampert, 2018
  *	Website: www.kampis-elektroecke.de
- *  File info: USB ADC example project for the AT90USB1287.
+ *  File info: USB mouse example for the AT90USB1287.
 
   GNU GENERAL PUBLIC LICENSE:
   This program is free software: you can redistribute it and/or modify
@@ -23,7 +23,7 @@
  */
 
 /** @file main.c
- *  @brief USB ADC example project for the AT90USB1287.
+ *  @brief USB mouse example for the AT90USB1287.
  *
  *  Software for the AT90USB1287 USB tutorial from
  *  https://www.kampis-elektroecke.de/avr/avr8/at90usb1287-usb/
@@ -31,21 +31,18 @@
  *  @author Daniel Kampert
  */
 
-#include "ADC/ADC.h"
 #include "USB/USB.h"
 #include "GPIO/GPIO.h"
-#include "TestDescriptor.h"
+#include "MouseDescriptor.h"
 
 #define LED0_RED						PORTD, 4
 #define LED0_GREEN						PORTD, 5
 
-void USB_DeviceTask(void);
+void Mouse_Task(void);
 
 void USB_Event_OnError(void);
 void USB_Event_EndOfReset(void);
 void USB_Event_ConfigurationChanged(const uint8_t Configuration);
-
-void ADC_Event_ConversionComplete(uint8_t Channel, uint16_t Result);
 
 const USB_DeviceCallbacks_t Events_USB =
 {
@@ -71,14 +68,6 @@ int main(void)
 	GPIO_SetDirection(GET_PERIPHERAL(LED0_GREEN), GET_INDEX(LED0_GREEN), GPIO_DIRECTION_OUT);
 
 	/*
-		Initialize the ADC
-			-> Internal 2.56 V reference
-			-> Prescaler 128
-			-> Enable interrupts
-	*/
-	ADC_Init(ADC_Event_ConversionComplete);
-
-	/*
 		Initialize the USB
 			-> USB device
 			-> Low-Speed
@@ -93,28 +82,15 @@ int main(void)
 	while(1) 
 	{
 	    USB_Poll();
-		USB_DeviceTask();
+		Mouse_Task();
 	}
 }
 
-void USB_DeviceTask(void)
+void Mouse_Task(void)
 {
 	if(USB_GetState() != USB_STATE_CONFIGURED)
 	{
 		return;
-	}
-
-	// Select the OUT endpoint
-	Endpoint_Select(OUT_EP);
-	if(Endpoint_IsReadWriteAllowed())
-	{
-		Endpoint_AckOUT();
-
-		// Set the new channel
-		ADC_SetChannel(Endpoint_ReadByte());
-
-		// Start a new ADC conversion
-		ADC_StartConversion();
 	}
 }
 
@@ -135,7 +111,7 @@ void USB_Event_OnError(void)
 void USB_Event_ConfigurationChanged(const uint8_t Configuration)
 {
 	// Configure the endpoints according to the configuration
-	if(Endpoint_Configure(IN_EP, ENDPOINT_TYPE_INTERRUPT, EP_SIZE, 1) && Endpoint_Configure(OUT_EP, ENDPOINT_TYPE_INTERRUPT, EP_SIZE, 1))
+	if(Endpoint_Configure(MOUSE_IN_EP, ENDPOINT_TYPE_INTERRUPT, MOUSE_EP_SIZE, 1))
 	{
 		// Set D2 red and green when the endpoint configuration was successfully
 		GPIO_Set(GET_PERIPHERAL(LED0_GREEN), GET_INDEX(LED0_GREEN));
@@ -144,26 +120,5 @@ void USB_Event_ConfigurationChanged(const uint8_t Configuration)
 	else
 	{
 		USB_Event_OnError();
-	}
-}
-
-void ADC_Event_ConversionComplete(const uint8_t Channel, const uint16_t Result)
-{
-	// Select the IN endpoint
-	Endpoint_Select(IN_EP);
-	if(Endpoint_IsReadWriteAllowed())
-	{
-		if(Endpoint_INReady())
-		{
-			// Transmit the ADC result
-			Endpoint_WriteByte(Result & 0xFF);
-			Endpoint_WriteByte((Result >> 0x08) & 0xFF);
-
-			Endpoint_FlushIN();
-		}
-		else
-		{
-			Endpoint_FlushIN();
-		}
 	}
 }
